8211a1b04ed2c23aafbc1eec7e991367a602f89e
Guillaume P <guillaume.pascal43@laposte.net>
2013-11-21 17:37:36 +0100

    Ajoute la possibilité de pousser un cube. Ajoute une rotation aux items.

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxController : MonoBehaviour {
+
+	private Vector3 position;
+	private bool movable;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+		movable = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * 10);
+		if ( (position - transform.position).magnitude < 0.01 ) {
+			movable = true;
+			transform.position = position;
+		}
+	}
+
+	public void move(Vector3 move)
+	{
+		if (movable) {
+			position = transform.position + move;
+			movable = false;
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	
-	public float walkSpeed = 6.0F;
+	public float walkSpeed = 4F;
+	public float speed = 0f;
 	public float rotationFactor = 5.0F;
     public float gravity = 20.0F;
 	
@@ -19,13 +20,21 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (controller.isGrounded) 
-		{
+		moveDirection = Vector3.zero;
+		if (controller.isGrounded) {
 			// Moves forward, left, right, backward
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= walkSpeed;
+			moveDirection = transform.TransformDirection(moveDirection);
+            
 		}
+		if (moveDirection != Vector3.zero) {
+			speed = Mathf.Lerp(speed, walkSpeed, Time.deltaTime * 5);
+		}
+		else {
+			speed = Mathf.Lerp(speed, 0, Time.deltaTime * 4);
+		}
+		moveDirection *= speed;
+
 		// Applies move
 	    moveDirection.y -= gravity * Time.deltaTime;
 	    controller.Move(moveDirection * Time.deltaTime);
@@ -35,5 +44,45 @@ public class PlayerController : MonoBehaviour {
 		rotation *= rotationFactor * Time.timeScale;
 		transform.Rotate(rotation);
 	}
-	
+
+	void OnTriggerEnter(Collider other)
+	{
+		OnTriggerEnterOrStay(other);
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		OnTriggerEnterOrStay(other);
+	}
+
+
+	void OnTriggerEnterOrStay(Collider other)
+	{
+		if (other.gameObject.tag == "item")
+		{
+			Destroy(other.gameObject);
+		}
+		else if (other.gameObject.tag == "box")
+		{
+			Vector3 move = other.gameObject.transform.position - transform.position;
+			float dotForward = Vector3.Dot (Vector3.forward, move);
+			float dotRight = Vector3.Dot (Vector3.right, move);
+			if (Mathf.Abs(dotForward) > Mathf.Abs(dotRight)) {
+				if (dotForward > 0)
+					move = Vector3.forward;
+				else
+					move = Vector3.back;
+			}
+			else {
+				if (dotRight > 0) 
+					move = Vector3.right;
+				else
+					move = Vector3.left;
+			}
+			
+			BoxController boxC = other.GetComponent<BoxController>();
+			boxC.move(move);
+			speed = 0;
+		}
+	}
 }
