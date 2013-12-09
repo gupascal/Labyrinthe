659e935e05c7d69d016ad15d252b0855eb94f94c
Guillaume P <guillaume.pascal43@laposte.net>
2013-12-09 14:52:19 +0100

    Ajoute une camera vue de dessus.

---
+++ Assets/Scripts/TopCameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopCameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F1))
+		{
+			GetComponent<Camera>().enabled = true;
+		}
+		if(Input.GetKeyUp(KeyCode.F1))
+		{
+			GetComponent<Camera>().enabled = false;
+		}
+	}
+}
