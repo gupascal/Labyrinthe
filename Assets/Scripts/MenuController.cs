581ae84b56bbf48aafad8045cab46fa565d46a0a
Guillaume P <guillaume.pascal43@laposte.net>
2014-01-07 19:27:56 +0100

    Ajoute un menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public bool inMenu = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if (!inMenu)
+			return;
+
+		float buttonWidth = 128f;
+		float buttonHeight = 32f;
+
+		int idButton = 0;
+
+		if (GUI.Button(	new Rect((Screen.width - buttonWidth)/2f,
+		                         0.3f*Screen.height + buttonHeight * 1.33f * idButton++,
+		                         buttonWidth,
+		                         buttonHeight),
+		               "Tutorial"))
+		{
+
+		}
+		if (GUI.Button(	new Rect((Screen.width - buttonWidth)/2f,
+		                         0.3f*Screen.height + buttonHeight * 1.33f * idButton++,
+		                         buttonWidth,
+		                         buttonHeight),
+		               "Level 1"))
+		{
+			
+		}
+		if (GUI.Button(	new Rect((Screen.width - buttonWidth)/2f,
+		                         0.3f*Screen.height + buttonHeight * 1.33f * idButton++,
+		                         buttonWidth,
+		                         buttonHeight),
+		               "Level 2"))
+		{
+			
+		}
+		if (GUI.Button(	new Rect((Screen.width - buttonWidth)/2f,
+		                         0.3f*Screen.height + buttonHeight * 1.33f * idButton++,
+		                         buttonWidth,
+		                         buttonHeight),
+		               "Quit"))
+		{
+			
+		}
+	}
+}
